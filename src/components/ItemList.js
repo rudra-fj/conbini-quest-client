51d9830b52ce75f32f9f358b2d8f74e5b479ae7f
Rudra <ps4.rudra@gmail.com>
2025-08-20 11:23:25 +1200

    Fix final API URL in ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -10,7 +10,7 @@ function ItemList() {
   // This useEffect hook runs once when the component loads.
   useEffect(() => {
     // We ask our backend server for the list of items.
-   axios.get('https://conbini-quest.onrender.com/api/items')
+   axios.get('https://conbini-quest-server.onrender.com/api/items')
       .then(response => {
         // If successful, we update our 'items' variable with the data.
         setItems(response.data);
